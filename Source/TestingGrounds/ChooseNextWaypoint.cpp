12809febeae237bfe74866ef0228dd70d75c7751
Huy Khoa Le <huyxkhoa@gmail.com>
2019-02-04 12:27:01 -0700

    cycle through patrol points using C++ instead of blueprints.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,14 +2,29 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" // TODO remove coupling
+
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	//TODO protect against empty patrol routes
 
-	
+	// Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolledPoints = PatrollingGuard->PatrolPointsCPP;
+
+	//set next waypoint
 	auto Blackboardcomp = OwnerComp.GetBlackboardComponent();
 	auto Index = Blackboardcomp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index %i"), Index);
+	Blackboardcomp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolledPoints[Index]);
+
+	//cycle the index
+
+	auto NextIndex = (Index + 1) % PatrolledPoints.Num();
+	Blackboardcomp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 	
 	return EBTNodeResult::Succeeded;
 
